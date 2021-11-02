1f9dfb00ca4dbaddb7bc3376c7e194e04130bc49
Laude Espiritu <laude.espiritu@hoolisoftware.com>
2021-11-02 14:45:52 +0800

    Initial commit

---
+++ vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img width="420" height="280" alt="Vue logo" src="./assets/socialwiz.png">
+  <HelloWorld msg="Welcome to SocialWiz Static Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
